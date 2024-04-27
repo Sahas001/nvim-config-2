a798d8c701407212bf5836465418f6302a2bb45c
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-27 10:21:22 +0545

    .

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -16,25 +16,25 @@ local sources = {
   b.formatting.gofumpt,
   b.formatting.goimports_reviser,
   b.formatting.golines,
-  b.diagnostics.golangci_lint,
+  -- b.diagnostics.golangci_lint,
 
   -- cpp
   b.formatting.clang_format,
 }
 
 local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({
-    group = augroup,
-    buffer = bufnr,
-  })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = augroup,
-    buffer = bufnr,
-    callback = function ( )
-      vim.lsp.buf.format({bufnr = bufnr})
-    end,
-  })
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds {
+      group = augroup,
+      buffer = bufnr,
+    }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { bufnr = bufnr }
+      end,
+    })
   end
 end
 
